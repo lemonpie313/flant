aa4f814481943468df93891dd5246f10ca74b632
SuhyeonJin <lofi0613@gmail.com>
2024-07-22 15:29:16 +0900

    [update] swagger 추가

--- final_project_backend/src/main.ts
+++ final_project_backend/src/main.ts
@@ -1,18 +1,48 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:3000', // 프론트엔드 주소
+    credentials: true,
+  });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3001;
+
+  // 글로벌 URL 프리픽스 설정
+  app.setGlobalPrefix('api', { exclude: ['/health-check'] });
+
+  // 글로벌 파이프라인 설정
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-    }),
+    })
   );
 
-  await app.listen(3000);
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Trello Clone')
+    .setDescription('API description of Trello project')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true, // 새로고침 시에도 JWT 유지
+      tagsSorter: 'alpha', // API 그룹 정렬 알파벳순
+      operationsSorter: 'alpha', // API 그룹내에서도 정렬 알파벳순
+    },
+  });
+
+  await app.listen(port);
 }
 
 bootstrap();
