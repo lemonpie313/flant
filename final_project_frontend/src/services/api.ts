4b45acbb08242213265d65334a1c68be46dbf172
soooomm00@gmail.com <soooomm00@gmail.com>
2024-08-16 22:22:58 +0900

    fix : liveApi 추가

--- final_project_frontend/src/services/api.ts
+++ final_project_frontend/src/services/api.ts
@@ -100,3 +100,10 @@ export const postApi = {
   like: (postId: string) => api.post(`/posts/${postId}/like`),
   comment: (postId: string, content: string) => api.post(`/posts/${postId}/comments`, { content }),
 };
+
+export const liveApi = {
+  createLive: (artistId: string, title: string, liveType: string) =>
+    api.post('/live', { artistId, title, liveType }),
+  findAllLives: (communityId: string) => api.get(`/live/community/${communityId}`),
+  watchLive: (liveId: number) => api.get(`/live/${liveId}`),
+};
