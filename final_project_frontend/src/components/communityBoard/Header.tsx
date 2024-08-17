51929fe64f185bf235b39fb1766d2e5d88559949
soooomm00@gmail.com <soooomm00@gmail.com>
2024-08-17 18:36:59 +0900

    feat : 프론트엔드 파일 분리 Related to : #167

---
+++ final_project_frontend/src/components/communityBoard/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-blue-500 p-4 text-white">
+      <h1 className="text-2xl font-bold">커뮤니티 게시판</h1>
+    </header>
+  );
+};
+
+export default Header;
