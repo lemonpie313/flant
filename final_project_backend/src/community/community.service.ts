f2b9cdf99afea51193b37d86b9b74d8144dcd47c
blueclorox <dehydrate92@gmail.com>
2024-08-18 19:40:50 +0900

    fix/ post find return
    반환값 조정

--- final_project_backend/src/community/community.service.ts
+++ final_project_backend/src/community/community.service.ts
@@ -113,7 +113,7 @@ export class CommunityService {
         postId: post.postId,
         nickname: post.communityUser.nickName,
         profileImage: post.communityUser.users.profileImage,
-        isArtist: post.artistId !== null,  // artistId가 존재하면 아티스트로 간주
+        isArtist: post.artistId !== null, // artistId가 존재하면 아티스트로 간주
         content: post.content,
         createdAt: post.createdAt,
         updatedAt: post.updatedAt,
@@ -125,11 +125,21 @@ export class CommunityService {
     });
   
 
-    return {
-      status: HttpStatus.OK,
-      message: MESSAGES.COMMUNITY.FINDONE.SUCCEED,
-      data: processedPosts,
-    };
+  // 필요한 커뮤니티 정보와 가공된 posts 데이터를 함께 반환
+  return {
+    status: HttpStatus.OK,
+    message: MESSAGES.COMMUNITY.FINDONE.SUCCEED,
+    data: {
+      communityId: existedCommunity.communityId,
+      communityName: existedCommunity.communityName,
+      communityLogoImage: existedCommunity.communityLogoImage,
+      membershipPrice: existedCommunity.membershipPrice,
+      createdAt: existedCommunity.createdAt,
+      updatedAt: existedCommunity.updatedAt,
+      deletedAt: existedCommunity.deletedAt,
+      posts: processedPosts, // 가공된 posts 배열
+    },
+  };
   }
 
   async updateCommunity(
