ae8e000a3f4fa81311f12e85f12c8bfd113451ec
blueclorox <dehydrate92@gmail.com>
2024-08-24 15:02:42 +0900

    chat comp hide

--- final_project_frontend/src/components/Layout.tsx
+++ final_project_frontend/src/components/Layout.tsx
@@ -5,7 +5,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div>
       {children}
-      <ChatComponent />
+      {/* < ChatComponent /> */}
     </div>
   );
 };
