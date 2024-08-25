692194e65b5e7834b44df2e31e9a02bd5827961e
leeminjun <mlnsuk0702@gmail.com>
2024-08-25 18:01:34 +0900

    fix : 자유멤버십 => 멤버십 변경

--- final_project_frontend/src/pages/CommunityBoard.tsx
+++ final_project_frontend/src/pages/CommunityBoard.tsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import {
-  authApi,
-  communityApi,
-  postApi,
-  commentApi,
-  communityUserApi,
-  membershipApi,
-} from "../services/api";
+import { authApi, communityApi, postApi, commentApi, communityUserApi, membershipApi } from "../services/api";
 import axios from "axios";
 import Header from "../components/communityBoard/Header";
 import PostForm from "../components/communityBoard/PostForm";
 import PostCard from "./../components/communityBoard/PostCard";
-import {
-  Community,
-  Post,
-  CommunityUser,
-  Membership,
-} from "../components/communityBoard/types";
+import { Community, Post, CommunityUser, Membership } from "../components/communityBoard/types";
 import "./board.scss";
 import CommunityNavigationHeader from "../components/communityBoard/CommunityNavigationHeader";
 import { PaymentPortone } from "./payments/paymentPortone";
@@ -67,9 +55,7 @@ const CommunityBoardTest: React.FC = () => {
 
   const fetchCommunityUsers = async () => {
     try {
-      const response = await communityUserApi.findCommunityUser(
-        Number(communityId)
-      );
+      const response = await communityUserApi.findCommunityUser(Number(communityId));
 
       if (response) {
         setCommunityUser(response.data);
@@ -182,9 +168,7 @@ const CommunityBoardTest: React.FC = () => {
     try {
       const response = await communityApi.findMy();
       const myCommunity = response.data.data;
-      setIsCommunityJoined(
-        myCommunity.some((c: any) => c.communityId === Number(communityId))
-      );
+      setIsCommunityJoined(myCommunity.some((c: any) => c.communityId === Number(communityId)));
     } catch (error) {
       console.error("커뮤니티 가입 여부 확인 오류:", error);
     }
@@ -281,11 +265,7 @@ const CommunityBoardTest: React.FC = () => {
     <div className="community-board">
       {community && (
         <>
-          <Header
-            communityName={community.communityName}
-            isLoggedIn={isLoggedIn}
-            handleLogout={handleLogout}
-          />
+          <Header communityName={community.communityName} isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
           <CommunityNavigationHeader />
         </>
       )}
@@ -313,9 +293,7 @@ const CommunityBoardTest: React.FC = () => {
           ) : (
             <div>
               <p>이 페이지를 보려면 로그인이 필요합니다.</p>
-              <button onClick={() => navigate("/login")}>
-                로그인 페이지로 이동
-              </button>
+              <button onClick={() => navigate("/login")}>로그인 페이지로 이동</button>
             </div>
           )}
         </div>
@@ -350,24 +328,18 @@ const CommunityBoardTest: React.FC = () => {
           )}
           <div className="right-sidebar-membership">
             {isCommunityJoined
-              ? "자유 멤버십에 가입해서 새로운 스케줄 소식을 받아보세요."
+              ? "멤버십에 가입해서 새로운 스케줄 소식을 받아보세요."
               : "커뮤니티에 가입해 소식을 받아보세요."}
             <button
               className="right-sidebar-join-button"
-              onClick={
-                isCommunityJoined ? handleMembershipJoin : handleJoinButtonClick
-              }
+              onClick={isCommunityJoined ? handleMembershipJoin : handleJoinButtonClick}
               disabled={isPaymentProcessing} // 결제 중일 때 버튼 비활성화
             >
               {isCommunityJoined ? "Membership 가입하기" : "커뮤니티 가입하기"}
             </button>
             {showPayment && (
               <PaymentPortone
-                amount={
-                  community?.membershipPrice !== undefined
-                    ? community.membershipPrice
-                    : 10000
-                }
+                amount={community?.membershipPrice !== undefined ? community.membershipPrice : 10000}
                 onPaymentSuccess={handlePaymentSuccess}
               />
             )}
